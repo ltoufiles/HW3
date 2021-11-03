4a1b923e5ef64ace1377d0522ff987976a4b6b8c
Liliana Toufiles <lilianatoufiles@gmail.com>
2021-11-02 20:26:19 -0400

    updated javascript

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,15 +1,74 @@
-var password=document.getElementById("button"); 
+ //DOM elements
+ //const resultEl = document.getElementById('password');
+
+ //document.getElementById('generate').addEventListener('click', () => {
+ // resultEl.value = generatePassword(lowercase, uppercase, numbers, symbols, plength);
+ // const textarea = document.createElement("textarea").placeholder = ps;
+ // const password = resultEl.value;
+
+  //if (!password) {
+  //  return;
+  //}
+
+ // textarea.value = password;
+//  document.body.appendChild(textarea);
+  
+ //});
+
+
+ //const randomFunc = {
+  // lower: getRandomLower,
+  // upper: getRandomUpper,
+  // number: getRandomNumber,
+  // symbol: getRandomSymbol
+ // };
+ var specialCharacters = "!@#$%^&*()"
+ var generatebtn = document.querySelector("#generate");
  
- var plength = prompt("How many characters would you like?");
+ function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
-  while (plength < 6 || plength > 128) {
-    plength = prompt("Length must be 6-128 characters");
-  }
+  passwordText.value = password;
 
+}
+
+ generatebtn.addEventListener("click", function () {
+ 
+ function generatePassword() {
+  enter = parseInt(prompt("Choose between 6 and 128 characters for your password."));
+
+if (!enter) {
+  alert("Sorry! You must make a selection.")
+} else if (enter < 6 || enter > 128) {
+
+    enter = parseInt(prompt("Length must be 6-128 characters"));
+
+} else {
+  //asks additional criteria questions after validated input
  var uppercase = confirm("Would you like to use uppercase letters?");
  var lowercase = confirm("Would you like to use lowercase letters?");
  var numbers = confirm("would you like to use numbers?");
  var symbols = confirm("would you like to use special characters?");
+
+ var functionArray = {
+  getNumbers: function() {
+    return String.fromCharCode(Math.floor(Math.random() * 10 + 48));
+  },
+
+  getLowerCases: function() {
+    return String.fromCharCode(Math.floor(Math.random() * 26 + 97));
+  },
+
+  getUpperCases: function() {
+    return String.fromCharCode(Math.floor(Math.random() * 26 + 65));
+  },
+
+  getSpecialCharacters: function() {
+    return specialCharacters[Math.floor(Math.random() * specialCharacters.length)]
+  }
+
+};
 
  while (!(uppercase || lowercase || numbers || symbols)) {
     alert("Sorry! You need to pick at least one character ...");
@@ -20,38 +79,6 @@ var password=document.getElementById("button");
     symbols = confirm("Special characters?");
  }
 
- //DOM elements
- const resultEl = document.getElementById('password');
-
- document.getElementById('button').addEventListener('click', () => {
-  resultEl.value = generatePassword(lowercase, uppercase, numbers, symbols, plength);
-
-
-document.getElementById('clipboard').addEventListener('click', () => {
-  const textarea = document.createElement('textarea');
-  const password = resultEl.value;
-
-  if (!password) {
-    return;
-  }
-
-  textarea.value = password;
-  document.body.appendChild(textarea);
-  textarea.select();
-  document.execCommand('copy');
-  textarea.remove();
-  alert('Password copied to clipboard');
- });
-});
-
- const randomFunc = {
-  lower: getRandomLower,
-  upper: getRandomUpper,
-  number: getRandomNumber,
-  symbol: getRandomSymbol
- };
-
- function generatePassword(lower, upper, number, symbol, length) {
   let generatedPassword = '';
   const typesCount = lower + upper + number + symbol;
   const typesArr = [{
@@ -65,7 +92,7 @@ document.getElementById('clipboard').addEventListener('click', () => {
   }].filter(item => Object.values(item)[0]);
 
    // create a loop
-   for (let i = 0; i < length; i += typesCount) {
+   for (let i = 0; i < entr; i++) {
     typesArr.forEach(type => {
       const funcName = Object.keys(type)[0];
       generatedPassword += randomFunc[funcName]();
@@ -94,4 +121,8 @@ function getRandomSymbol() {
 return rando('!@#$%^&*(){}[]=<>/,.');
 }
 
+function UserInput(ps) {
+  document.getElementById("password").textContent = ps;
+
+}
  };
